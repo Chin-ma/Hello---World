9a4b41c2d7a04949aa90f5387af961e43a9f0172
Chin-ma <50634176+Chin-ma@users.noreply.github.com>
2019-06-24 18:35:18 +0530

    Add files via upload

---
+++ main03.py
@@ -0,0 +1,9 @@
+import time;
+localtime = time.asctime(time.localtime(time.time()))
+print("Local current time : ", localtime)
+
+print("SECOND PROJECT!!!!!!")
+import calendar
+cal = calendar.month(2019,6)
+print("Here's the calendar:")
+print(cal)
